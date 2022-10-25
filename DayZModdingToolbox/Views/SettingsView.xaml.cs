7b0f501bce9c7738deb341ef03a1ad968b7ae715
Alexander Schaaf <alexander.schaaf@incoe.de>
2022-10-25 15:28:09 +0200

    Drag&Drop enabled for mods

--- DayZModdingToolbox/Views/SettingsView.xaml.cs
+++ DayZModdingToolbox/Views/SettingsView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+using DayZModdingToolbox.ViewModels;
 
 namespace DayZModdingToolbox.Views
 {
@@ -10,6 +15,29 @@ namespace DayZModdingToolbox.Views
         public SettingsView()
         {
             InitializeComponent();
+        }
+
+        private void ModGrid_Drop(object sender, DragEventArgs e)
+        {
+            base.OnDrop(e);
+
+            // If the DataObject contains string data, extract it.
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Parsing dropped files
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                for (int i = 0; i < files.Length ; i++)
+                {
+                    ((SettingsViewModel)this.DataContext).Mods.Add(new() { ModPath = files[i] });
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void ModGrid_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
